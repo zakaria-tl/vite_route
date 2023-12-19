b66f6386c70ed9e0bf23d1a8d4d477f25e3223db
kyros-2 <zakariawidady74@gmail.com>
2023-12-19 21:43:43 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+
+function App() {
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/about' element={<About />} />
+    </Routes>
+  )
+}
+
+export default App

d0eb6d797a15dcd5cdba1c9901c324b94a44b22b
kyros-2 <zakariawidady74@gmail.com>
2023-12-24 18:13:01 +0100

    oejoie

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import "./App.css";
-import { HashRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 
 function App() {
   return (
-    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
       </Routes>
-    </HashRouter>
   );
 }
 

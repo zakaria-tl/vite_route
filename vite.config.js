90bb5a36c9c4ca4ef1afa52d494ddf8661e78044
kyros-2 <zakariawidady74@gmail.com>
2023-12-24 18:15:37 +0100

    final test

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  rollupOptions: {
-    external: ["react", "react-router", "react-router-dom"],
-    output: {
-      globals: {
-        react: "React",
+  build : {
+    rollupOptions : {
+      external : ["react", "react-router-dom"],
+      output: {
+        globals: {
+          react: "React",
+        },
       },
-    },
-  },
+    }
+  }
 })
